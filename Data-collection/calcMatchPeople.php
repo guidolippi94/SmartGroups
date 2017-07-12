1172326228eef699f94d53b7b73dd1bac32c8d8c
Sicarix <alessandro.soci@stud.unifi.it>
2017-07-12 18:20:41 +0200

    foreach idfacebook

--- Data-collection/calcMatchPeople.php
+++ Data-collection/calcMatchPeople.php
@@ -9,15 +9,25 @@
 session_start();
 include('../config.php');
 
-echo "nome: ".$_SESSION['nome'];
+$idF = $_SESSION['idFacebook'];
 
-$query = "SELECT * FROM user_point WHERE id_facebook='10213427321783189'";
+$query = "SELECT * FROM user_point WHERE id_facebook='$idF'";
 $user_point=$db->query($query);
- $rows = $user_point->fetch_assoc();
+$rows = $user_point->fetch_assoc();
 
 var_dump($rows);
-
 echo json_encode($rows);
+
+$query = "SELECT id_facebook, music, sport, food, travel, fashion, education, entertainment, healtcare FROM user_point";
+$user_point=$db->query($query);
+$rows2 = $user_point->fetch_all();
+foreach ($rows2 as $item){
+    if($rows['id_facebook'] != $item[0]){
+        var_dump($item);
+
+    }
+}
+
 ?>
 
 
