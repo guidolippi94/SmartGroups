2bb300fdd63ee976466a63cf2a3507764e5584fb
Sicarix <alessandro.soci@stud.unifi.it>
2017-07-13 15:52:43 +0200

    foreach idfacebook

--- Data-collection/calcMatchPeople.php
+++ Data-collection/calcMatchPeople.php
@@ -15,22 +15,6 @@ $query = "SELECT * FROM user_point WHERE id_facebook='$idF'";
 $user_point=$db->query($query);
 $currentPerson = $user_point->fetch_assoc();
 
-function personMatchDistance($currentPerson, $personFromDB){
-    $total=0;
-    $total += pow($currentPerson['music']-$personFromDB[1], 2);
-    $total += pow($currentPerson['sport']-$personFromDB[2], 2);
-    $total += pow($currentPerson['food']-$personFromDB[3], 2);
-    $total += pow($currentPerson['travel']-$personFromDB[4], 2);
-    $total += pow($currentPerson['fashion']-$personFromDB[5], 2);
-    $total += pow($currentPerson['education']-$personFromDB[6], 2);
-    $total += pow($currentPerson['entertainment']-$personFromDB[7], 2);
-    $total += pow($currentPerson['healtcare']-$personFromDB[8], 2);
-
-    $total = pow($total, 0.5);
-
-    return $total;
-}
-
 
 $matchArray = array();
 
@@ -41,13 +25,13 @@ $result=$db->query($queryDBUsersPoint);
 $personFromDB = [];
 while($row = $result->fetch_assoc()){
     $personFromDB[]=$row;
-/*    echo "Row print : ";
-    var_dump($row);*/
+   echo "Row print : ";
+    var_dump($row);
 }
 
 
 foreach ($personFromDB as $item){
-    if($currentPerson['id_facebook'] != $item[0]){
+    if($currentPerson['id_facebook'] != $item['id_facebook']){
         array_push($matchArray, personMatchDistance($currentPerson, $item));
     }
 }
@@ -55,6 +39,23 @@ foreach ($personFromDB as $item){
 $_SESSION['calculated_distance'] = $matchArray;
 
 var_dump($matchArray);
+
+
+function personMatchDistance($currentPerson, $personFromDB){
+    $total=0;
+    $total += pow($currentPerson['music']-$personFromDB['music'], 2);
+    $total += pow($currentPerson['sport']-$personFromDB['sport'], 2);
+    $total += pow($currentPerson['food']-$personFromDB['food'], 2);
+    $total += pow($currentPerson['travel']-$personFromDB['travel'], 2);
+    $total += pow($currentPerson['fashion']-$personFromDB['fashion'], 2);
+    $total += pow($currentPerson['education']-$personFromDB['education'], 2);
+    $total += pow($currentPerson['entertainment']-$personFromDB['entertainment'], 2);
+    $total += pow($currentPerson['healtcare']-$personFromDB['healtcare'], 2);
+
+    $total = pow($total, 0.5);
+
+    return $total;
+}
 
 ?>
 
