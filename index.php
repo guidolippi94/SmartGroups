930b694c5b54096a5f2c3d5b91c53d001f7c1a3c
Sicarix <alessandro.soci@stud.unifi.it>
2017-07-26 17:53:39 +0200

    calendar (manca la parte di javascript)

--- index.php
+++ index.php
@@ -10,13 +10,17 @@
 session_start();
 //var_dump($_SESSION);
 
+$id_facebook = $_SESSION['idFacebook'];
+
+
 // verifico di aver fatto il login
-if (!isset($_SESSION['idFacebook']) || !is_numeric($_SESSION ['idFacebook']) || $_SESSION['idFacebook'] == 0) {
+if (!isset($_SESSION['idFacebook']) || !is_numeric($_SESSION['idFacebook']) || $_SESSION['idFacebook'] == 0) {
     ?>
     <script>window.location = "login.php";</script>
     <?php
 }
 
+include ('Data-collection/query_events.php');
 
 
 
@@ -34,6 +38,8 @@ if (!isset($_SESSION['idFacebook']) || !is_numeric($_SESSION ['idFacebook']) || 
     <link rel="stylesheet" type="text/css" href="style/demo.css" />
     <link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.min.css" />
     <link rel="stylesheet" type="text/css" href="style/index_style.css" />
+    <link rel="stylesheet" type="text/css" href="style/calendar-style.css" />
+
     <script src="js/snap.svg-min.js"></script>
     <script src="Jquery-3.2.1.min/jquery-3.2.1.min.js"></script>
     <!--[if IE]>
@@ -42,6 +48,8 @@ if (!isset($_SESSION['idFacebook']) || !is_numeric($_SESSION ['idFacebook']) || 
 </head>
 <body>
 <div class="container">
+
+    <!-- dovrebbe venire fuori quello laterale -->
     <div class="menu-wrap">
         <nav class="menu">
             <div class="profile">
@@ -68,15 +76,75 @@ if (!isset($_SESSION['idFacebook']) || !is_numeric($_SESSION ['idFacebook']) || 
     <button class="menu-button" id="open-button"></button>
     <div class="content-wrap">
         <div class="top-site">
-            <div style="width:100%; height: 14px"></div>
+            <div style="width:100%; height: 7px"></div>
             <div class="top-site-end">
                 <p> <b>SMART GROUPS </b></p>
             </div>
-        </div>
-        <div class="today-event" id="today"></div>
-    </div>
+            <div style="height: 62px"></div>
+            <div class="calendar">
+                <div class="month">
+                    <ul>
+                        <li class="prev">&#10094;</li>
+                        <li class="next">&#10095;</li>
+                        <li style="text-align:center">
+                            August<br>
+                            <span style="font-size:18px">2016</span>
+                        </li>
+                    </ul>
+                </div>
 
+                <ul class="weekdays">
+                    <li>Mo</li>
+                    <li>Tu</li>
+                    <li>We</li>
+                    <li>Th</li>
+                    <li>Fr</li>
+                    <li>Sa</li>
+                    <li>Su</li>
+                </ul>
+
+                <ul class="days">
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                    <li>4</li>
+                    <li>5</li>
+                    <li>6</li>
+                    <li>7</li>
+                    <li>8</li>
+                    <li>9</li>
+                    <li><span class="active">10</span></li>
+                    <li>11</li>
+                    <li>12</li>
+                    <li>13</li>
+                    <li>14</li>
+                    <li>15</li>
+                    <li>16</li>
+                    <li>17</li>
+                    <li>18</li>
+                    <li>19</li>
+                    <li>20</li>
+                    <li>21</li>
+                    <li>22</li>
+                    <li>23</li>
+                    <li>24</li>
+                    <li>25</li>
+                    <li>26</li>
+                    <li>27</li>
+                    <li>28</li>
+                    <li>29</li>
+                    <li>30</li>
+                    <li>31</li>
+                </ul>
+            </div>
+        </div>
+        <!-- <div class="today-event" id="today"></div> -->
+
+    </div>
 </div><!-- /container -->
+
+
+
 <script src="scriptINDEX.js"></script>
 <script src="js/classie.js"></script>
 <script src="js/main3.js"></script>
--- scriptINDEX.js
+++ scriptINDEX.js
@@ -5,6 +5,9 @@
 function isEmpty( el ){
     return !$.trim(el.html())
 }
+
+
+
 if (isEmpty($('#today'))) {
     $('#today').text('Oggi non hai eventi');// do something
 }
